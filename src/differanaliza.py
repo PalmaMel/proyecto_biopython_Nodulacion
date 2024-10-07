133c1ad719b37405d14b644bd4b0c517f4d4f735
DanielZBV <danielzb@lcg.unam.mx>
2024-10-07 16:55:51 -0600

    Primera version de esta cosa, no funciona

---
+++ src/differanaliza.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_tropici = pd.read_excel("./data/Complete repository Nodulation.xlsx", sheet_name="Nod21NE")  
+df_giardini = pd.read_excel("./data/Complete repository Nodulation.xlsx", sheet_name="Nod21NI")  
+
+nitrogen_genes = ['nifH', 'nodA', 'nodB']  
+df_filtered_tropici = df_tropici[df_tropici['ID'].isin(nitrogen_genes)]
+df_filtered_giardini = df_giardini[df_giardini['ID'].isin(nitrogen_genes)]
+
+comparison = df_filtered_tropici.set_index('ID')[['log2FoldChange']].rename(columns={'log2FoldChange': 'Tropici'})
+comparison['Giardini'] = df_filtered_giardini.set_index('ID')['log2FoldChange']
+comparison['Difference'] = comparison['Tropici'] - comparison['Giardini']
+
+comparison[['Tropici', 'Giardini']].plot(kind='bar', figsize=(10, 6))
+plt.title("Comparación de la expresión génica: R. tropici vs R. giardini")
+plt.ylabel("log2FoldChange")
+plt.show()
