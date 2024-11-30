cd66c89d16452703e2365a2f3802fd65a9ddc7c4
DanielZBV <danielzb@lcg.unam.mx>
2024-11-30 03:27:08 -0600

    Se hace saca la gráfica de N5vsNI

--- src/differanaliza.py
+++ src/differanaliza.py
@@ -31,20 +31,45 @@ file_N5vsNE = './data/N5vsNE.xlsx'
 file_NEvsNI = './data/NEvsNI.xlsx'
 # Se leen los archivos excel con pandas 
 N5vsNE = pd.read_excel(file_N5vsNE, sheet_name="O'Rourke_AddFile14_NEvN5")
+NEvsNI = pd.read_excel(file_NEvsNI, sheet_name="O'Rourke_AddFile15_NEvNI")
+
 # Se seleccionan las columnas de interes: 'GeneID', 'NE', 'N5' y 'FoldChange'
 N5vsNE = N5vsNE[['GeneID', 'NE', 'N5', 'FoldChange']]
+NEvsNI = NEvsNI[['GeneID', 'NE', 'NI', 'FoldChange']]
 
 # ===Creacion de Grafico===
 
 # Se crea unu gráfico de dispersion basado en el DataFrame
 # Utilizamos 'GeneID' como eje X y 'FoldChange' como eje Y.
-fig = px.scatter(N5vsNE, 
+figN5vsNE = px.scatter(N5vsNE, 
                 x='GeneID', # Columna de identificadores de genes (eje X)
                 y='FoldChange', # Columna para el cambio de expresion (cambio de expresion)
                 title='Comparacion de Patrones de Expresion: N5 vs NE', # Titulo
-                labels={ # Etiquetas para los ejes
+                labels={ # Etiquetas para los ejes 
                     'GeneID': 'ID del Gen', 'FoldChange': 'Nivel de Expresion'})
-fig.update_traces(marker=dict(size=5, color='purple')) # Aspectos graficos: size de los puntos y color
+figN5vsNE.update_traces(marker=dict(size=5, color='purple')) # Aspectos graficos: size de los puntos y color
 
 # Mostrar la grafica interactiva en el navegador 
-fig.show()
+figN5vsNE.show()
+
+figNEvsNI = px.scatter(NEvsNI,
+                       x='GeneID',
+                       y= 'FoldChange',
+                       title= 'Log2Fold Change: NE vs NI',
+                       labels={
+                           'GeneID': 'ID del Gen', 'FoldChange': 'Log2Fold Change'})
+figNEvsNI.show()
+
+combi = pd.merge(N5vsNE, NEvsNI, on= 'GeneID', suffixes=('_N5vsNE', '_NEvsNI'))
+combi = combi[['GeneID', 'FoldChange_N5vsNE', 'FoldChange_NEvsNI']]
+combi['FoldChange_N5vsNI'] = combi['FoldChange_N5vsNE'] + combi['FoldChange_NEvsNI']
+
+N5vsNI = combi[['GeneID', 'FoldChange_N5vsNI']]
+
+figN5vsNI = px.scatter(N5vsNI,
+                       x='GeneID',
+                       y= 'FoldChange_N5vsNI',
+                       title= 'Log2Fold Change: N5 vs NI',
+                       labels={
+                           'GeneID': 'ID del Gen', 'FoldChange': 'Log2Fold Change'})
+figN5vsNI.show()
